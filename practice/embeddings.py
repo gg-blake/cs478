e7f3322c84d2a1eea438b42aa69f69f3137a43f3
Blake Moody <blake092202@gmail.com>
2024-09-09 18:06:59 -0400

    add embeddings practice file and document

--- practice/embeddings.py
+++ practice/embeddings.py
@@ -1,7 +1,15 @@
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core import Settings
+from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
 
 # Loads a small embedding model from HuggingFace that can be run locally
 Settings.embed_model = HuggingFaceEmbedding(
     model_name="BAAI/bge-small-en-v1.5"
 )
+
+documents = SimpleDirectoryReader("./data").load_data()
+
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+
+response = query_engine.query("query string")
