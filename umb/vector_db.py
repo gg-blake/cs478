ea9ab3a53d17890a1764e36d0bd63542b6549fc1
Blake Moody <blake092202@gmail.com>
2024-10-12 22:39:26 -0400

    attention head constructed

--- umb/vector_db.py
+++ umb/vector_db.py
@@ -21,120 +21,61 @@ def cross_entropy(y0, x, e):
     return loss
         
 
-class Embedding:
-    def __init__(self, v_count, p_count):
-        self.v_count = v_count
-        self.p_count = p_count
-        self.data = torch.rand(v_count, p_count, device=device)
-
-    def __call__(self, idx):
-        output = self.data[idx]
-        output.unsqueeze(0)
-        return output
-    
-    def cosine_similarity(self, i, j):
-        assert i < len(self.data) and j < len(self.data)
-
-cosine_similarity = nn.CosineSimilarity(dim=2)
-def loss_fn(output, target):
-    return -cosine_similarity(output, target).mean()
-
-class NNLM(nn.Module):
-    def __init__(self, tokenizer, p_count):
+class AttentionHead(nn.Module):
+    def __init__(self, head_size, embed_size):
         super().__init__()
-        self.embeddings = nn.Embedding(len(tokenizer._vocab), len(tokenizer._vocab), dtype=torch.half, device=device)
+        self.embed_size = embed_size
+        self.query_weights = nn.Linear(embed_size, head_size, bias=False, device=device)
+        self.key_weights = nn.Linear(embed_size, head_size, bias=False, device=device)
+        self.value_weights = nn.Linear(embed_size, head_size, bias=False, device=device)
+
+    def forward(self, embeddings):
+        # Queries store the information of what other embeddings have in a particular block
+        query = self.query_weights(embeddings)
+        # Keys store the information that a particular embedding has relative to other embeddings in a block
+        key = self.key_weights(embeddings)
+        # By multiplying the keys and queries together, we can allow the embeddings to influence the meaning of other embeddings in the block
+        # We need to sqrt(embed_size) to ensure the softmax of wei doesn't get to spiky
+        wei = query @ key.transpose(-2, -1) * self.embed_size**-0.5 
+        # When training a model, we don't want embeddings that are ahead of an embedding in a block to send information to it (its like cheating in a test)
+        # So we will apply a mask to wei
+        tril = torch.tril(torch.ones(embeddings.shape[1], embeddings.shape[1], device=device))
+        wei = wei.masked_fill(tril == 0, float('-inf'))
+        # Then we apply a softmax to make the output on interval [0,1)
+        wei = torch.softmax(wei, dim=-1)
+        # We don't apply the embeddings directly to wei but instead we apply another backpropagatable linear layer to the embeddings (called value) and then apply wei
+        value = self.value_weights(embeddings)
+        return wei @ value
+        
+
+class LanguageModel(nn.Module):
+    def __init__(self, vocab_size, embedding_size, block_size):
+        super().__init__()
+        self.token_embeddings = nn.Embedding(vocab_size, embedding_size, device=device)
+        self.positional_embeddings = nn.Embedding(block_size, embedding_size, device=device)
+        self.attention_head = AttentionHead(embedding_size, embedding_size)
+        self.lm_head = nn.Linear(embedding_size, vocab_size, bias=False, device=device)
 
     def forward(self, idx, targets=None):
-        logits = self.embeddings(idx)
-        '''outputs = self.embeddings(targets)
-
+        B, T = idx.shape
+        token_idx = self.token_embeddings(idx)
+        positional_idx = self.positional_embeddings(torch.arange(T, device=device))
         
-        if targets is None:
-            loss = None
-        else:
-            loss = loss_fn(logits, outputs)'''
+        positioned_token_embedding = token_idx + positional_idx
+        attended_logits = self.attention_head(positioned_token_embedding)
+        logits = self.lm_head(attended_logits)
         if targets is not None:
             B, T, C = logits.shape
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
-        else:
-            loss = None
 
         return logits, loss
     
-    def generate(self, idx, max_new_tokens):
-        for _ in range(max_new_tokens):
-            logits, loss = self(idx)
-            logits = logits[:, -1, :]
-            probs = F.softmax(logits, dim=-1)
-            idx_next = torch.multinomial(probs, num_samples=1)
-            idx = torch.cat((idx, idx_next), dim=1)
+    def generate(self, idx, max):
+        for _ in range(max):
+            pass
 
-        return idx
-
-        
-
-'''class VectorDB:
-    def __init__(self, tokenizer, embedding_size):
-        self.tokenizer = tokenizer
-        self.embedding_size = embedding_size
-        self._vectors = torch.zeros((0, embedding_size))
-        self._vectors_index = Graph()
-
-    def train_from_document(self, file_paths):
-        cat_text = ""
-        for file_path in file_paths:
-            with open(file_path) as f:
-                cat_text += f.read()
-
-        self.tokenizer.train(cat_text, self.embedding_size)
-        for file_path in file_paths:
-            with open(file_path) as f:
-                text = f.read()
-                self.tokenizer.encode(text)
-                tokens = torch.tensor(self.tokenizer.encode(text))
-                token_freq = self._token_frequency(tokens)
-                self.add_vector(token_freq)
-
-    def _token_frequency(self, tokens):
-        token_freq = torch.zeros(len(self.tokenizer._vocab))
-        for token in tokens:
-            token_freq[token] += 1
-
-        return token_freq.softmax(dim=0)
-    
-    def add_vector(self, vector):
-        # Add the vector to the database (vector should be normalized with softmax)
-        self._vectors_index.node(str(len(self._vectors)))
-        if len(self._vectors) == 0:
-            self._vectors = torch.cat((self._vectors, vector.unsqueeze(0)))
-            return
-        
-        similarity_vector = F.cosine_similarity(vector, self._vectors, dim=1)
-        for i, similarity in enumerate(similarity_vector):
-            self._vectors_index.edge(str(len(self._vectors)), str(i), label=str(similarity.item()))
-
-        self._vectors = torch.cat((self._vectors, vector.unsqueeze(0)))
-
-    def find_similar_text(self, text, number_of_results=5):
-        vector = self._token_frequency(torch.tensor(self.tokenizer.encode(text)))
-
-        # Find the most similar vectors in the database
-        similarity_vector = F.cosine_similarity(vector, self._vectors, dim=1)
-        similarity_vector = similarity_vector.argsort(descending=True)
-        return similarity_vector[:number_of_results]
-    
-    def visualize(self):
-        self._vectors_index.render("graph.png")
-
-if __name__ == "__main__":
-    tokenizer = Tokenizer()
-    db = VectorDB(tokenizer, 3000)
-    db.train_from_document(["data/d1.txt", "data/d2.txt", "data/d3.txt"])
-    db.visualize()
-    search_text = "Financial aid and the bursars office and student loans"
-    print(db._vectors)'''
 
 def sample(data, batch_size, block_size):
     starting_indices = torch.randint(len(data) - block_size, (batch_size,))
@@ -154,28 +95,20 @@ if __name__ == "__main__":
     tokenizer.load("tokenizer_models/umb100k-1.model")
     vocab_size = len(tokenizer._vocab)
 
-    model = NNLM(tokenizer, 200)
-    emb = Embedding(len(tokenizer._vocab), 200)
-    logits, loss = model.forward(s, t)
-    print(logits)
-    print(tokenizer.decode([20000, 31024, 1014]))
-    idx = torch.zeros((1, 1), dtype=torch.long, device=device)
-    print(model.generate(idx, 100)[0].tolist())
-    print(tokenizer.decode(model.generate(idx, 100)[0].tolist()))
-
+    lm = LanguageModel(vocab_size, 32, 8)
     
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-    loss = None
-    for steps in range(1000):
-        xb, yb = sample(tokens, 4, 8)
-        logits, loss = model(xb, yb)
-        optimizer.zero_grad(set_to_none=True)
+    optimizer = adamw.AdamW(lm.parameters(), lr=1e-3)
+    for epoch in range(100):
+        optimizer.zero_grad()
+        logits, loss = lm(s, t)
         loss.backward()
         optimizer.step()
-        
+        print(loss.item())
 
-    print(loss.item())
- 
+    torch.save(lm.state_dict(), "umb100k-1.model")
+    print("Model saved")
+
+    
     
         
         
