d3dea75becd71564d84c6e9b5f73b3c0913df14d
Blake Moody <blake092202@gmail.com>
2024-11-12 02:04:46 -0500

    hard part of audit parser finally complete

---
+++ umb/audit_data/build.py
@@ -0,0 +1,113 @@
+"""
+Name : build_catalog.py
+Description : Scraper tool for pulling all the UMB course catalog, formatting it and saving it to JSON for RAG pipelines
+Author : Blake Moody
+Date : 11-8-2024
+"""
+from pypdf import PdfReader
+import regex as re
+import json
+import sys
+
+def extract_as_txt(filename):
+    reader = PdfReader(f"pdf/{filename}.pdf")
+    number_of_pages = len(reader.pages)
+
+    with open(f"txt/{filename}.txt", "w") as f:
+        for i in range(number_of_pages):
+            page = reader.pages[i]
+            text = page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False)
+            text = re.sub(re.compile("\s+Page\s[0-9]+\sof\s[0-9]+(\s+)?"), "", text)
+            f.write(text)
+        
+        f.close()
+
+def extract_sections_by_status(text):
+    pattern = re.compile(f"(NO|OK|\+\-)\s+([A-Za-z0-9]+( [A-Za-z0-9]+)+)\s+\*+")
+    '''keys = [x[0] for x in pattern.findall(text)]
+    values = re.split(pattern, text)'''
+    matches = list(re.finditer(pattern, text))
+
+    return matches
+
+def extract_section(text, pattern):
+    
+    '''keys = [x[0] for x in pattern.findall(text)]
+    values = re.split(pattern, text)'''
+    s = list(re.finditer(pattern, text))
+    matches = [x.start() for x in s]
+    matches_shifted = matches[1:] + [len(text)]
+    zipped = [text[m0:m1] for m0, m1 in zip(matches, matches_shifted)]
+    
+    return zip(s, zipped)
+
+def add_entry(group_list):
+    data = {
+        "term": group_list[1],
+        "course": group_list[2],
+        "credits": group_list[4],
+        "grade": group_list[5],
+        "title": " ".join(str(group_list[7]).split()),
+        "type": " ".join(str(section_key).split()),
+        "subtype": " ".join(str(entry_title).split())
+    }
+    entry_data.append(data)
+
+if __name__ == "__main__":
+    audit_name = sys.argv[1]
+    extract_as_txt(audit_name)
+    text = ""
+    with open(f"txt/{audit_name}.txt", "r") as f:
+        text = f.read()
+    f.close()
+    entry_data = []
+    text = "\n".join(re.split(re.compile("\s+Page\s[0-9]+\sof\s[0-9]+\s+"), text))
+    
+    
+    section_pattern = r"(NO|OK|\+\-)\s+(((\s+[A-Z0-9\:\&\/\']+)+)(\s+\*+(\s+(\s+([A-Z0-9\:\&\/\']+))+)?)?)"
+    
+    subsection_pattern = r"(\s{3,}|\n\s*|GPA)(\-|\+|ip\s*(\-|\+))(\s+R\s)?(\s+[0-9]\))?\s*(([A-Z0-9(\/)\:\']([A-Za-z0-9\/\:\'\-]+\s)*)([A-Za-z0-9\/\:\-\']+))(\s{3,}|\n\s*)"
+    entry_pattern = r"(([a-zA-Z0-9]{4})\s+([a-zA-z]+\s?[0-9]+([a-zA-Z]?)+)\s+([0-9]+\.[0-9]+)\s+([A-Za-z\-\+\/]+)\s+(\>S|\>X|\>\-|RP|\>D|\>R)?\s+((([\w\&\/]+\s)+)?\w+(\n|[^.])?))"
+    sections = extract_section(text, section_pattern)
+    validation_length = len([_ for _ in extract_section(text, entry_pattern)])
+
+    for section_obj, section_str in sections:
+        subsections = extract_section(section_str, subsection_pattern)
+        section_key = section_obj.groups()[2]
+        has_subsection = False
+        
+        for subsection_obj, subsection_str in subsections:
+            has_subsection = True
+            entry_title = subsection_obj.groups()[5]
+            
+            entries = extract_section(subsection_str, entry_pattern)
+            for entry_obj, entry_str in entries:
+                group_list = list(entry_obj.groups())
+                add_entry(group_list)
+            
+        if has_subsection:
+            continue
+
+        entries = extract_section(section_str, entry_pattern)
+        entry_title = section_key
+        for entry_obj, entry_str in entries:
+            group_list = list(entry_obj.groups())
+            add_entry(group_list)
+        
+    with open(f"json/{audit_name}.json", "w+") as outfile: 
+        json.dump(entry_data, outfile, indent=4)
+
+    if len(entry_data) != validation_length:
+        print(f"Warning: Failed to parse {validation_length - len(entry_data)} entries")
+        print("\tTotal entries:", len(entry_data), "Expected entries:", validation_length, "\n")
+    else:
+        print("Parsing complete! ", end="")
+
+    print("Saved as", f"json/{audit_name}.json")
+    
+
+# (NO|OK|\+\-)\s+(((\s+[A-Z0-9\:\&\/\']+)+)(\s+\*+(\s+(\s+([A-Z0-9\:\&\/\']+))+)?)?)
+# ((NO|OK|\+\-)\s+(((\s+[A-Z0-9\:\&\/\']+)+)(\s+\*+(\s+(\s+([A-Z0-9\:\&\/\']+))+)?)?)|(\n\s|\s\s|\n)(ip\s)?(\-|\+)\s{2,}([0-9]+\))?\s?([A-Za-z\/\-\:0-9]+\s(\-\s)?)+)\s(^(?:(?!((NO|OK|\+\-)\s+([A-Za-z0-9]+( [A-Za-z0-9]+)+)\s+\*+|(\n\s|\s\s|\n)(ip\s)?(\-|\+)\s{2,}([0-9]+\))?\s?([A-Za-z\/\-\:0-9]+\s(\-\s)?)+)).)*$\n){0,}
+# ((NO|OK|\+\-)\s+([A-Za-z0-9]+( [A-Za-z0-9]+)+)\s+\*+|(\n\s|\s\s|\n)(ip\s)?(\-|\+)\s{2,}([0-9]+\))?\s?([A-Za-z\/\-\:0-9]+\s(\-\s)?)+)\s(^(?:(?!((NO|OK|\+\-)\s+([A-Za-z0-9]+( [A-Za-z0-9]+)+)\s+\*+|(\n\s|\s\s|\n)(ip\s)?(\-|\+)\s{2,}([0-9]+\))?\s?([A-Za-z\/\-\:0-9]+\s(\-\s)?)+)).)*$\n){0,}
+# (\s{3,}|\n\s*)(\-|\+|ip\s*(\-|\+))(\s+R\s)?(\s+[0-9]\))?\s*([A-Za-z0-9]+)(\s[A-Za-z0-9]+)*(\s{3,}|\n\s*)
+# (\s{3,}|\n\s*)(\-|\+|ip\s*(\-|\+))(\s+R\s)?(\s+[0-9]\))?\s*(([A-Z0-9(\/)\:\']([A-Za-z0-9\/\:\'\-]+\s)*)([A-Za-z0-9\/\:\-\']+))(\s{3,}|\n\s*)
--- umb/course_data/extract_urls.py
+++ umb/course_data/extract_urls.py
@@ -1,11 +1,27 @@
+"""
+Name : extract_urls.py
+Description : Tool to extract urls from a page
+Author : Blake Moody
+Date : 11-8-2024
+"""
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.parse import quote
 
-def extract_urls(url):
+def extract_urls(url, keyword=None):
+    """
+    Returns a list of all urls contained on web page 
+
+    Parameters
+    ----------
+    url : str
+        Web page URL to be scraped for urls
+    """
     page = urlopen(url).read()
     soup = BeautifulSoup(page)
     soup.prettify()
-    return [anchor['href'] for anchor in soup.findAll('a', href=True)]
-
-if __name__ == "__main__":
-    print(extract_urls('https://courses.umb.edu/course_catalog/subjects/2025%20Spring'))
+    if keyword is None:
+        return [quote(anchor['href']).replace("%3A", ":") for anchor in soup.findAll('a', href=True)]
+    else:
+        return [quote(anchor['href']).replace("%3A", ":") for anchor in soup.findAll('a', href=True) if keyword in anchor['href']]
