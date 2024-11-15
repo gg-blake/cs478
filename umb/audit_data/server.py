c3e1e8c0c60a8b47f97e34dc962e390eafab1195
ayden <aydendiel@gmail.com>
2024-11-15 10:17:36 -0500

    docs: added swagger to the audit_data api

--- umb/audit_data/server.py
+++ umb/audit_data/server.py
@@ -2,12 +2,58 @@ from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 import magic
+from flask_swagger_ui import get_swaggerui_blueprint
 from audit_parser import extract_as_txt_from_request_file, parse_audit_text
 
 app = Flask(__name__)
 
+# Configure Swagger UI
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Audit Report Parser API",
+        'spec_version': '2.0'  # Changed to Swagger 2.0
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
 @app.route('/parse_audit_report', methods=['POST'])
 def parse_audit_report():
+    """
+    Parse an audit report PDF file
+    ---
+    tags:
+      - Audit Report
+    consumes:
+      - multipart/form-data
+    parameters:
+      - in: formData
+        name: file
+        type: file
+        required: true
+        description: The PDF file to parse
+    responses:
+      200:
+        description: PDF processed successfully
+        schema:
+          properties:
+            message:
+              type: string
+              example: PDF processed successfully
+            data:
+              type: object
+              description: Parsed audit report data
+      400:
+        description: Invalid input
+        schema:
+          properties:
+            error:
+              type: string
+              example: No file provided
+    """
     if not request.files:
         return jsonify({'error': 'No file provided'}), 400
         
